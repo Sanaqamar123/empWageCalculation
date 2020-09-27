ced5cbf78968953ade5d55c7434243ac3bf4a773
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-27 17:07:43 +0530

    Check attendance of Employee

--- empWageCalculation.java
+++ empWageCalculation.java
@@ -1,5 +1,15 @@
 public class empWageCalculation {
+
 	public static void main(String[] args) {
+
 	System.out.println("Welcome to Employee Wage Calculation");
+
+	double num1 = Math.floor(Math.random()*10)%2;
+
+		if(num1 == 0) {
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
 	}
 }
